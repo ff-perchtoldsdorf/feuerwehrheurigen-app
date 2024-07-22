4adb6449c042cda6252068883aafba1275417801
JPromi <jonasmpr05@gmail.com>
2024-07-22 09:00:32 +0200

    add user

--- src/app/components/assets/header/header.component.ts
+++ src/app/components/assets/header/header.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { User } from '../../../dtos/auth';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'asset-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  constructor(
+    private authService: AuthService
+  ) {}
+
+  public user: User | null = null;
+
+  environment = environment;
+
+  ngOnInit() {
+    this.getUserInformation();
+  }
+
+  private getUserInformation() {
+    this.authService.userInformation().subscribe(
+      (data: User) => {
+        this.user = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  userMenu(open: boolean) {
+    const userMenu = document.getElementById('user-menu');
+    if (open) {
+      userMenu?.classList.add('show');
+    } else {
+      userMenu?.classList.remove('show');
+    }
+  }
+
+  logout() {
+    window.location.href = environment.domain.auth + '/logout';
+  }
 
 }
